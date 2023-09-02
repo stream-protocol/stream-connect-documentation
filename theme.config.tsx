a082692b8236227d143c68c3f2a9b8f1df029463
StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-09-02 08:09:17 +0300

    added new pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>StreamConnect Doc</span>,
   project: {
     link: 'https://github.com/stream-protocol/stream-connect-documentation',
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/stream-protocol/stream-connect-documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'StreamPayments Development Documentation',
   },
 }
 
